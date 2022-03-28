1c87c9d15ff98a8bfd8ebea3c717509001f70707
Uladzislau Khamkou <kham@tuta.io>
2022-03-28 11:09:26 +0300

    add basic structure of solver logic

---
+++ crosssword.py
@@ -0,0 +1,13 @@
+def set_value(crossword, i, value):
+    return NotImplemented
+
+
+def dp(crossword):
+    # find the line with least number of possible value
+    # for optin in optins: dp(set_value(crossword, line, optin)
+    return NotImplemented
+
+
+def solve(crossword):
+    # convert start info into table of possible values
+    return dp(crossword)
